528b8d2dd1d5e587d48dd7985f8bd4bf1bce5ff2
Travis CI User <travis@example.org>
2017-07-11 12:13:10 +0000

    Documentation for hasufell/rust-libnotify@cee315c

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["error_chain"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"error_chain/example_generated/inner/struct.Error.html\" title=\"struct error_chain::example_generated::inner::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"error_chain/example_generated/struct.Error.html\" title=\"struct error_chain::example_generated::Error\">Error</a>",];
+implementors["gdk_pixbuf"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glib/error/struct.Error.html\" title=\"struct glib::error::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glib/error/struct.BoolError.html\" title=\"struct glib::error::BoolError\">BoolError</a>",];
+implementors["glib"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glib/error/struct.Error.html\" title=\"struct glib::error::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glib/error/struct.BoolError.html\" title=\"struct glib::error::BoolError\">BoolError</a>",];
+implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/sys/imp/ext/fs/trait.MetadataExt.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["error_chain"] = [];
+implementors["gdk_pixbuf"] = [];
+implementors["glib"] = [];
+implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
